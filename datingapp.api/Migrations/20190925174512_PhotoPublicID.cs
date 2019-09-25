eb212fbed4b1773f9067c627ea8456aa7368f5a9
Phil Mosely <moselyphil@gmail.com>
2019-09-25 14:41:27 -0400

    Partial Progress on Photo upload

---
+++ datingapp.api/Migrations/20190925174512_PhotoPublicID.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace datingapp.api.Migrations
+{
+    public partial class PhotoPublicID : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PublicID",
+                table: "Photos",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PublicID",
+                table: "Photos");
+        }
+    }
+}
